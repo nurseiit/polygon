4e2b372d16eadc2426bf5c686cfa03f171d44e7e
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2024-03-25 22:43:59 +0000

    wc: step 1

--- wc/src/main.rs
+++ wc/src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+
+/// a simple wc
+#[derive(Parser)]
+struct Cli {
+    /// print the byte counts
+    #[arg(short = 'c', long, default_value_t = false)]
+    bytes: bool,
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+
+    let bytes_count = content.bytes().len();
+
+    println!("{} {}", bytes_count, args.path.to_str().unwrap())
 }
