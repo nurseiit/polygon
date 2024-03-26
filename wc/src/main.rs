7923998232ec19ce74992b140a9903d533ef6415
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2024-03-26 19:56:05 +0000

    refactor

--- wc/src/main.rs
+++ wc/src/main.rs
@@ -37,36 +37,35 @@ fn main() -> Result<()> {
     let args = Cli::parse();
 
     let content = std::fs::read_to_string(&args.path)
-        .with_context(|| format!("could not read file `{}`", args.path.display()))?;
-
-    let lines_count = content.lines().count();
-    let words_count = content.split_whitespace().count();
-    let char_count = content.chars().count();
-    let bytes_count = content.bytes().len();
+        .with_context(|| format!("{}: No such file or directory", args.path.display()))?;
 
     let show_all = !args.chars & [args.lines, args.words, args.bytes].iter().all(|&x| !x);
 
     let mut counts_to_show: Vec<usize> = Vec::new();
 
     if args.lines || show_all {
+        let lines_count = content.lines().count();
         counts_to_show.push(lines_count);
     }
 
     if args.words || show_all {
+        let words_count = content.split_whitespace().count();
         counts_to_show.push(words_count);
     }
 
     if args.chars {
+        let char_count = content.chars().count();
         counts_to_show.push(char_count);
     }
 
     if args.bytes || show_all {
+        let bytes_count = content.bytes().len();
         counts_to_show.push(bytes_count);
     }
 
     if !counts_to_show.is_empty() {
-        let largest_count = counts_to_show.iter().max().unwrap_or(&0);
-        let largest_width = log10(*largest_count);
+        let largest_count = *counts_to_show.iter().max().unwrap_or(&0);
+        let largest_width = log10(largest_count);
         let out = counts_to_show
             .iter()
             .map(|count| format!("{:>width$}", count, width = largest_width))
